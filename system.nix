57b8a8e3270c4313ddc21a04389c6e0317250eef
bokiledobri <bokiledobri@gmail.com>
2026-04-18 00:35:18 +0200

    NixOS gen: 2026-04-18 00:35:18

--- system.nix
+++ system.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }:
 
 {
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot";
